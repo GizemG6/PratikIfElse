2a7f0ae9e10c998a2c5e4791031f450c089f463d
cang8 <0.gizemgunes@gmail.com>
2024-09-30 11:50:44 +0300

    Add project files.

---
+++ PratikIfElse/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Lütfen bir sayı giriniz:");
+long sayi = int.Parse(Console.ReadLine());
+
+if (sayi > 10)
+{
+    Console.WriteLine("Girilen sayi 10'dan büyüktür.");
+}
+else if (sayi == 10)
+{
+    Console.WriteLine("Girilen sayi 10'dur.");
+}
+else
+{
+    Console.WriteLine("Girilen sayi 10'dan kücüktür.");
+}
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Girilen sayi cifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayi tektir");
+}
